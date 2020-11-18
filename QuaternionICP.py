5b0e1748777066383898598f0a17225148c64660
liyouwang <1085066875@qq.com>
2020-11-18 14:16:41 +0800

    修复模板点集与带配准点集计算协方差矩阵的位置先后错误

--- QuaternionICP.py
+++ QuaternionICP.py
@@ -5,6 +5,7 @@ date: 11/12/2020
 """
 import numpy as np
 import cv2
+import matplotlib.pyplot as plt
 
 
 def calculate_scale(template_points, register_points):
@@ -15,6 +16,7 @@ def calculate_scale(template_points, register_points):
     :return:
     """
     scale = 1
+
     return scale
 
 
@@ -49,13 +51,40 @@ def icp_svd(template_points, register_points):
     R = rotation_matrix
     t = transformation_matrix
     return R, t
-    
+
+
+def quaternion_2_rotation_matrix(q):
+    """
+    四元数转化为旋转矩阵
+    :param q:
+    :return: 旋转矩阵
+    """
+    rotation_matrix = np.array([[np.square(q[0]) + np.square(q[1]) - np.square(q[2]) - np.square(q[3]),
+                                 2 * (q[1] * q[2] - q[0] * q[3]), 2 * (q[1] * q[3] + q[0] * q[2])],
+                                [2 * (q[1] * q[2] + q[0] * q[3]),
+                                 np.square(q[0]) - np.square(q[1]) + np.square(q[2]) - np.square(q[3]),
+                                 2 * (q[2] * q[3] - q[0] * q[1])],
+                                [2 * (q[1] * q[3] - q[0] * q[2]), 2 * (q[2] * q[3] + q[0] * q[1]),
+                                 np.square(q[0]) - np.square(q[1]) - np.square(q[2]) + np.square(q[3])]],
+                               dtype=np.float32)
+    return rotation_matrix
+
+
+def rotation_matrix_2_quaternion(rotation_matrix):
+    """
+    旋转矩阵转化为四元数
+    :param rotation_matrix:
+    :return:
+    """
+    quaternion = None
+    return quaternion
+
 
 def icp_quaternion(template_points, register_points):
     """
     计算两个点集之间的icp 配准
     参考https://blog.csdn.net/hongbin_xu/article/details/80537100
-    博客中的旋转矩阵写错了,需要按照论文里面的修改 而且要转转置
+    博客中的旋转矩阵写错了,需要按照论文里面的修改
     :param template_points: 模板点集 N 3
     :param register_points: 带配准点集 N 3
     :return:
@@ -65,7 +94,7 @@ def icp_quaternion(template_points, register_points):
     row, col = template_points.shape
     mean_template_points = np.mean(template_points, axis=0)
     mean_register_points = np.mean(register_points, axis=0)
-    cov = (template_points - mean_template_points).T.dot((register_points - mean_register_points)) / row
+    cov = (register_points - mean_register_points).T.dot((template_points - mean_template_points)) / row
     A = cov - cov.T
     delta = np.array([A[1, 2], A[2, 0], A[0, 1]], dtype=np.float32).T
     Q = np.zeros((4, 4), dtype=np.float32)
@@ -78,7 +107,7 @@ def icp_quaternion(template_points, register_points):
     print("四元数是{}".format(q))
     rotation_matrix = np.array([[np.square(q[0]) + np.square(q[1]) - np.square(q[2]) - np.square(q[3]), 2 * (q[1] * q[2] - q[0] * q[3]), 2 * (q[1] * q[3] + q[0] * q[2])],
                                 [2 * (q[1] * q[2] + q[0] * q[3]), np.square(q[0]) - np.square(q[1]) + np.square(q[2]) - np.square(q[3]), 2 * (q[2] * q[3] - q[0] * q[1])],
-                                [2 * (q[1] * q[3] - q[0] * q[2]), 2 * (q[2] * q[3] + q[0] * q[1]), np.square(q[0]) - np.square(q[1]) - np.square(q[2]) + np.square(q[3])]], dtype=np.float32).T
+                                [2 * (q[1] * q[3] - q[0] * q[2]), 2 * (q[2] * q[3] + q[0] * q[1]), np.square(q[0]) - np.square(q[1]) - np.square(q[2]) + np.square(q[3])]], dtype=np.float32)
     print("旋转矩阵是{}".format(rotation_matrix))
     tranform_matrix = mean_template_points - rotation_matrix.dot(mean_register_points)
     print("计算出来的平移向量是{}".format(tranform_matrix))
@@ -91,12 +120,16 @@ def icp_quaternion(template_points, register_points):
 
 
 if __name__ == "__main__":
-    test_points = np.random.rand(10, 3)
+    n_number = 10
+    test_points = np.random.rand(n_number, 3)
     rotation_vector = np.array([1, 1, 2], dtype=np.float32)
     rotation_matrix = cv2.Rodrigues(rotation_vector)[0]
     print("gt rotation_matrix is {}".format(rotation_matrix))
     transform_vector = np.array([2, 2, 2], dtype=np.float32)
     print("gt tranformation vector {}".format(transform_vector))
     register_points = rotation_matrix.dot(test_points.T).T + transform_vector
+    mu, sigma = 0, 0.1
+    white_noise = np.random.normal(mu, sigma, size=(n_number, 3))
+    # register_points = register_points + white_noise  # add noise
     icp_quaternion(register_points, test_points)
     icp_svd(register_points, test_points)
